40cdcff156f100a69b1acbc4fc977dc15d75df06
big555ddd <biggafu5@hotmail.com>
2025-04-11 09:54:17 +0700

    helper

--- app/helper/helper.go
+++ app/helper/helper.go
@@ -1,14 +1,25 @@
 package helper
 
 import (
+	"encoding/json"
+
 	"github.com/gin-gonic/gin"
 )
 
-func GetUserByToken(ctx *gin.Context) (any, error) {
+type user struct {
+	ID int64 `json:"id"`
+}
+
+func GetUserByToken(ctx *gin.Context) (int64, error) {
 	claims, exist := ctx.Get("claims")
 	if !exist {
 		return 0, nil
 	}
+	var user user
+	err := json.Unmarshal(claims.([]byte), &user)
+	if err != nil {
+		return 0, err
+	}
 
-	return claims, nil
+	return user.ID, nil
 }
